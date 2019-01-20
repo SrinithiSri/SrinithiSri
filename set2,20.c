9c94ce86f00e6bcadc3b6e004134c7189cd1cfba
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-01-20 20:22:21 +0530

    Create set2,20.c

---
+++ set2,20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,mul=1,i;
+    printf("enter n");
+    scanf("%d",&n);
+for(i=1;i<=5;i++)
+{
+    mul=n*i;
+    printf("%d ",mul);
+}
+    return 0;
+}
