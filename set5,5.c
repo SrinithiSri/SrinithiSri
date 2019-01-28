eab405ca1ba78e83105a28bfa207d44b5b5f7d7b
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-01-28 21:54:56 +0530

    Create set5,5.c

---
+++ set5,5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    long int n;
+    int c=0;
+    scanf("%ld",&n);
+    if(n>=0 && n<=100000000000)
+    {
+    while(n>0)
+    {
+        n=n/10;
+        c++;
+    }
+    printf("%d",c);
+    }
+    return 0;
+}
