da94c54d4a5175e45c6d8284ea530bdef04de4d0
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-01-28 20:15:23 +0530

    Create set3,9.c

---
+++ set3,9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[50],n,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d %d\n",a[i],i);
+    }
+
+return 0;
+}
