aa63ff107a11f13b6b84b7c6cb1e10949c88731d
SrinithiSri <46066288+SrinithiSri@users.noreply.github.com>
2019-02-05 19:56:24 +0530

    Create set7,9.c

---
+++ set7,9.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+     int n,k,d;
+     scanf("%d %d",&n,&k);
+     d=n-k;
+         if(d%2==0)
+         {
+             printf("even");
+         }
+         else
+         {
+             printf("odd");
+         }
+
+return 0;
+}
